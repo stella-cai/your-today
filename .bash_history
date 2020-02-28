ec441da42d40e79e9a467a381a83885d033f03b7
Pan Chen <pan.chen@mail.utoronto.ca>
2020-02-27 22:25:15 -0500

    asd

---
+++ .bash_history
@@ -0,0 +1,10 @@
+ls
+cd ../
+cd website
+ls
+nano config.php
+cd ../
+ls
+cd chensnotes.com
+ls
+cd ../
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,6 @@
 function welcome() {
     let today = new Date();
-    let currentHour = today.getHours;
+    let currentHour = today.getHours();
     let welcomeText = '';
     if (5 <= currentHour && currentHour < 12) {
         welcomeText = 'Good Morning!';
@@ -15,11 +15,4 @@ function welcome() {
         welcomeText = 'Good Night!';
     }
     document.querySelector('#welcome').innerHTML = welcomeText;
-}
-
-
-function footerTime()
-{
-	var date = new Date(); 
-	document.getElementById("footerInfo").innerHTML = "Copyright &copy; " + date.getFullYear() + " Go Today." + " All Rights Reserved.</br>The data of this website is stored in Montreal.</br>";
 }
