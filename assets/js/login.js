23b7612290e8a4d7ed72f9f7f8597f5aa331865b
KarenChen <kareny.chen@mail.utoronto.ca>
2020-02-28 04:06:11 -0500

    p1: added login.js

---
+++ assets/js/login.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const loginSubmit = document.querySelector('#login-button')
+loginSubmit.addEventListener('click', login)
+
+function login(e) {
+    e.preventDefault()
+    console.log('pressed the login button!')
+
+    if(checkPassword()) {
+        console.log('login success')
+        redirect()
+    } else {
+        console.log('login failed')
+    }
+}
+
+function checkPassword() {
+    const username = document.querySelector("#login-username").value
+    const password = document.querySelector("#login-password").value
+
+    console.log(username)
+    console.log(password)
+
+    if(username == 'user' && password == 'user') {
+        return true
+    }
+    return false
+}
+
+function redirect(){
+    console.log("redirecting");
+    window.location = "index.html";
+}
