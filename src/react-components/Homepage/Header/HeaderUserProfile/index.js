520aef7574f1bcdeb754ee3618187b4cec975888
Pan Chen <pan.chen@mail.utoronto.ca>
2020-03-01 18:02:04 -0500

    user can change mood now

--- src/react-components/Homepage/Header/HeaderUserProfile/index.js
+++ src/react-components/Homepage/Header/HeaderUserProfile/index.js
@@ -4,25 +4,59 @@ import "./styles.css";
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
 import Button from '@material-ui/core/Button';
 import {toggleDrawer} from '../../../../actions/drawers'
+import UserStateModal from './UserStateModal'
 
 export default function HeaderUserProfile(props) {
   const username = props.username;
   const state = props.state;
   const setState = props.setState;
+  const [profileState, setProfileState] = useState(0);
   //const [state, setState] = useState(props.state);
+
+  const [userStateModalOpen, setUserStateModalOpen] = useState(false);
+
+  const getProfileState = () => 
+  {
+      if(profileState == 0) {
+        return(
+          <SentimentVerySatisfiedIcon className="profile-status"></SentimentVerySatisfiedIcon>
+        )
+      }
+      else if (profileState == 1) {
+        return(
+          <SentimentVeryDissatisfiedIcon className="profile-status"></SentimentVeryDissatisfiedIcon>
+        )
+      }
+  }
+
+  const handleOpen = () => {
+    setUserStateModalOpen(true);
+  };
+
+  const handleClose = () => {
+    setUserStateModalOpen(false);
+  };
 
   return(
     <div className="header-profile">
+      <UserStateModal
+      userStateModalOpen = {userStateModalOpen}
+      setUserStateModalOpen = {setUserStateModalOpen}
+      handleClose = {handleClose}
+      profileState = {profileState}
+      setProfileState = {setProfileState} 
+      >
+      </UserStateModal>
       <Grid container direction="row" alignItems="center">
         <Grid item>
           <Button onClick={()=>toggleDrawer('left', state, setState, true)}><Avatar>H</Avatar></Button>
         </Grid>
         <Grid item className="profile-username">{username}</Grid>
-
+        <Button onClick={handleOpen}>{getProfileState()}</Button>
         <Grid item>
-          <SentimentVerySatisfiedIcon className="profile-status"></SentimentVerySatisfiedIcon>
         </Grid>
       </Grid>
     </div>
