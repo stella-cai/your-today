903acd4a114bf37ca2146170395bdeebfd765d9e
Pan Chen <pan.chen@mail.utoronto.ca>
2020-03-03 18:52:20 -0500

    ad

---
+++ src/react-components/Admin/AdminPage/index.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Creation from './Creation'
+import Request from './Request'
+import Frozen from './Frozen'
+import All from './All'
+import Feedback from './Feedback'
+import { mergeClasses } from '@material-ui/styles';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`nav-tabpanel-${index}`}
+      aria-labelledby={`nav-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `nav-tab-${index}`,
+    'aria-controls': `nav-tabpanel-${index}`,
+  };
+}
+
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={event => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundImage: 'url(https://source.unsplash.com/T_Qe4QlMIvQ)',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    height: '100vh'
+  },
+}));
+
+export default function AdminPage() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  // lATER WE WILL FETCH ALL THE DATA FROM THE SERVER.
+
+  // Recent Creation.
+  const [creation, setCreation] = React.useState(
+    [
+      { id: 1, user: "Tom", date: "2020-03-03" },
+      { id: 2, user: "Jack", date: "2020-03-02" },
+    ]
+  )
+
+  // Recent Creation.
+  const [request, setRequest] = React.useState(
+    [
+      { id: 3, user: "Lucy", reason: "I forgot my passowrd", date: "2020-03-03" },
+      { id: 4, user: "Maria", reason: "I registered my account last month.", date: "2020-03-02" },
+    ]
+  )
+
+
+  // Frozen.
+  const [frozen, setFrozen] = React.useState(
+    [
+      { id: 5, user: "Peter", reason: "Sending spam emails" },
+    ]
+  )
+
+  // All Active Accounts.
+  const [all, setAll] = React.useState(
+    [
+      { id: 1, user: "Tom" },
+      { id: 2, user: "Jack" },
+      { id: 3, user: "Lucy" },
+      { id: 4, user: "Maria" },
+    ]
+  )
+
+  // Feedback.
+  const [feedback, setFeedback] = React.useState(
+    [
+      { id: 1, user: "Jack", content: "Good" },
+      { id: 2, user: "Jack", content: "Wonderful" },
+      { id: 3, user: "Lucy", content: "So so" },
+    ]
+  )
+
+  const removeFromScreen = (index, method, data) => {
+    const newData = [...data];
+    newData.splice(index, 1);
+    console.log(newData)
+    method(newData);
+  }
+
+  const addToScreen = (method, data, item) => {
+    const newData = [...data, item];
+    method(newData);
+  }
+
+
+
+  return (
+    <div classname={classes.root}>
+      <Box className={classes.tabs}>
+        <AppBar position="static">
+          <Tabs
+            variant="fullWidth"
+            value={value}
+            onChange={handleChange}
+            aria-label="Admin Page Tabs"
+          >
+            <LinkTab label="Recent Creations" href="/" {...a11yProps(0)} />
+            <LinkTab label="Requests" href="/" {...a11yProps(1)} />
+            <LinkTab label="Frozen Accounts" href="/" {...a11yProps(2)} />
+            <LinkTab label="All Accounts" href="/" {...a11yProps(3)} />
+            <LinkTab label="Feedbacks" href="/" {...a11yProps(4)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          <Creation removeFromScreen={removeFromScreen} creation={creation} setCreation={setCreation}></Creation>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Request removeFromScreen={removeFromScreen} request={request} setRequest={setRequest}></Request>
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <Frozen all={all} setAll={setAll} addToScreen={addToScreen} removeFromScreen={removeFromScreen} frozen={frozen} setFrozen={setFrozen}></Frozen>
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <All frozen={frozen} setFrozen={setFrozen} addToScreen={addToScreen} removeFromScreen={removeFromScreen} all={all} setAll={setAll}></All>
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          <Feedback removeFromScreen={removeFromScreen} feedback={feedback} setFeedback={setFeedback}></Feedback>
+        </TabPanel>
+      </Box>
+    </div>
+  );
+}
--- src/react-components/Admin/index.js
+++ src/react-components/Admin/index.js
@@ -1,176 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
+/*  Full Queue component */
+// Everything here was previously in the App component.
+import React, { useState } from "react";
 import Box from '@material-ui/core/Box';
-import Creation from './Creation'
-import Request from './Request'
-import Frozen from './Frozen'
-import All from './All'
-import Feedback from './Feedback'
-import { mergeClasses } from '@material-ui/styles';
+import AdminPage from './AdminPage'
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+// Importing components
+import Header from "../Homepage/Header";
 
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`nav-tabpanel-${index}`}
-      aria-labelledby={`nav-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `nav-tab-${index}`,
-    'aria-controls': `nav-tabpanel-${index}`,
-  };
-}
-
-function LinkTab(props) {
-  return (
-    <Tab
-      component="a"
-      onClick={event => {
-        event.preventDefault();
-      }}
-      {...props}
-    />
-  );
-}
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundImage: 'url(https://source.unsplash.com/T_Qe4QlMIvQ)',
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    height: '100vh'
-  },
-  tabs: {
-    width: '50%',
-    height: '50%'
-  }
-}));
 
 export default function Admin() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  // lATER WE WILL FETCH ALL THE DATA FROM THE SERVER.
-
-  // Recent Creation.
-  const [creation, setCreation] = React.useState(
+  const [links, setLinks] = useState(
     [
-      { id: 1, user: "Tom", date: "2020-03-03" },
-      { id: 2, user: "Jack", date: "2020-03-02" },
+      {url:"https://www.google.com", name:"Google"},
+      {url:"http://acorn.utoronto.ca", name:"Acorn"},
+      {url:"http://q.utoronto.ca", name:"Quercus"},
+      {url:"https://web.cs.toronto.edu", name:"UofT CS"},
+      {url:"https://leetcode.com", name:"Leetcode"}
     ]
-  )
+  );
 
-  // Recent Creation.
-  const [request, setRequest] = React.useState(
-    [
-      { id: 3, user: "Lucy", reason: "I forgot my passowrd", date: "2020-03-03" },
-      { id: 4, user: "Maria", reason: "I registered my account last month.", date: "2020-03-02" },
-    ]
-  )
-
-
-  // Frozen.
-  const [frozen, setFrozen] = React.useState(
-    [
-      { id: 5, user: "Peter", reason: "Sending spam emails" },
-    ]
-  )
-
-  // All Active Accounts.
-  const [all, setAll] = React.useState(
-    [
-      { id: 1, user: "Tom" },
-      { id: 2, user: "Jack" },
-      { id: 3, user: "Lucy" },
-      { id: 4, user: "Maria" },
-    ]
-  )
-
-  // Feedback.
-  const [feedback, setFeedback] = React.useState(
-    [
-      { id: 1, user: "Jack", content: "Good" },
-      { id: 2, user: "Jack", content: "Wonderful" },
-      { id: 3, user: "Lucy", content: "So so" },
-    ]
-  )
-
-  const removeFromScreen = (index, method, data) => {
-    const newData = [...data];
-    newData.splice(index, 1);
-    console.log(newData)
-    method(newData);
+  const bgStyle = () => {
+    return {
+      backgroundImage: 'url(https://source.unsplash.com/T_Qe4QlMIvQ)',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      height: '100vh'
+    }
   }
 
-  const addToScreen = (method, data, item) => {
-    const newData = [...data, item];
-    method(newData);
-  }
-
-
-
-  return (
-    <div classname={classes.root}>
-      <Box className={classes.tabs}>
-        <AppBar position="static">
-          <Tabs
-            variant="fullWidth"
-            value={value}
-            onChange={handleChange}
-            aria-label="Admin Page Tabs"
-          >
-            <LinkTab label="Recent Creations" href="/" {...a11yProps(0)} />
-            <LinkTab label="Requests" href="/" {...a11yProps(1)} />
-            <LinkTab label="Frozen Accounts" href="/" {...a11yProps(2)} />
-            <LinkTab label="All Accounts" href="/" {...a11yProps(3)} />
-            <LinkTab label="Feedbacks" href="/" {...a11yProps(4)} />
-          </Tabs>
-        </AppBar>
-        <TabPanel value={value} index={0}>
-          <Creation removeFromScreen={removeFromScreen} creation={creation} setCreation={setCreation}></Creation>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <Request removeFromScreen={removeFromScreen} request={request} setRequest={setRequest}></Request>
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          <Frozen all={all} setAll={setAll} addToScreen={addToScreen} removeFromScreen={removeFromScreen} frozen={frozen} setFrozen={setFrozen}></Frozen>
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          <All frozen={frozen} setFrozen={setFrozen} addToScreen={addToScreen} removeFromScreen={removeFromScreen} all={all} setAll={setAll}></All>
-        </TabPanel>
-        <TabPanel value={value} index={4}>
-          <Feedback removeFromScreen={removeFromScreen} feedback={feedback} setFeedback={setFeedback}></Feedback>
-        </TabPanel>
-      </Box>
+  return(      
+    <div style={bgStyle()}>
+      <div className="App">
+        <Box style={{position:'relative', paddingLeft:"50px", paddingRight: '50px', paddingTop:'30px'}}>
+          <Header
+            username="John Doe"
+            links = {links} 
+            setLinks = {setLinks}
+          />
+        </Box>
+        <AdminPage></AdminPage>
+      </div>
     </div>
   );
 }
