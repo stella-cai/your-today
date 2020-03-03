0c819f1c0b0572d936bb12aec82e97c546d9a756
Stella <stella.sijiacai@gmail.com>
2020-03-03 14:31:43 -0500

    admin features

---
+++ src/react-components/AdminPage/Header/Feedback/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Feedback extends React.Component {
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Feedback;
