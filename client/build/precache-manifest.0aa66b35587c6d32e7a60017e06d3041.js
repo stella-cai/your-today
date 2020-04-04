2d03e7336140506c59446801233894b286f40ac4
Pan Chen <pan.chen@mail.utoronto.ca>
2020-04-04 17:11:59 -0400

    pushed

---
+++ client/build/precache-manifest.0aa66b35587c6d32e7a60017e06d3041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff92136f053eb64f343af0ff14263b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6307b2e689295904f926",
+    "url": "/static/css/main.5ed32802.chunk.css"
+  },
+  {
+    "revision": "e73e7f13f8f89a7644c4",
+    "url": "/static/js/2.778b9740.chunk.js"
+  },
+  {
+    "revision": "6307b2e689295904f926",
+    "url": "/static/js/main.ea776196.chunk.js"
+  },
+  {
+    "revision": "4121286d667ea1a436e4",
+    "url": "/static/js/runtime-main.1d9e47dd.js"
+  }
+]);
