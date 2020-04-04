0bd2468053cf3ec5c72221e023e76d94a0ec5cc3
Stella <stella.sijiacai@gmail.com>
2020-04-04 18:32:58 -0400

    admin logout

--- client/src/Admin/Header/RightDrawer/RightDrawer.jsx
+++ client/src/Admin/Header/RightDrawer/RightDrawer.jsx
@@ -3,9 +3,8 @@ import Drawer from '@material-ui/core/Drawer';
 import { toggleDrawer } from '../../../actions/drawers'
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-// import LinkSetting from './LinkSetting';
-// import WallpaperSetting from './WallpaperSetting';
-// import ContactSetting from './ContactSetting';
+import {Middleware} from "../../../actions/middleware";
+
 
 
 import './RightDrawer.css';
@@ -50,38 +49,6 @@ export function RightDrawer(props) {
   const state = props.state;
   const setState = props.setState;
   const classes = useStyles();
-  const links = props.links;
-  const setLinks = props.setLinks;
-  const wallpaper = props.wallpaper;
-  const setWallpaper = props.setWallpaper;
-
-  const [linkSettingOpen, setLinkSettingOpen] = useState(false);
-  const [wallpaperSettingOpen, setWallpaperSettingOpen] = useState(false);
-  const [contactSettingOpen, setContactSettingOpen] = useState(false);
-
-  const handleOpen = (e) => {
-    e.preventDefault();
-    switch(e.target.parentElement.id){
-      case("linkBtn"):
-        setLinkSettingOpen(true);
-        break;
-      case("wallpaperBtn"):
-        console.log("click wallpaper")
-        setWallpaperSettingOpen(true);
-        break;
-      case("contactBtn"):
-        setContactSettingOpen(true);
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleClose = () => {
-    setLinkSettingOpen(false);
-    setWallpaperSettingOpen(false);
-    setContactSettingOpen(false);
-  };
 
   const sideList = side => (
     <div className={classes.root}>
@@ -89,11 +56,7 @@ export function RightDrawer(props) {
         <span className={classes.header}>Setting</span>
       </div>
       <div className={classes.buttons}>
-        {/* <Button onClick={handleOpen} variant="outlined" size="large" id="wallpaperBtn" className={classes.button}>Wallpaper</Button>
-        <Button onClick={handleOpen} variant="outlined" id="linkBtn" size="large" className={classes.button}>Favourite Links</Button> */}
-        {/* <Button variant="outlined" size="large" className={classes.button}>Manage Features</Button> */}
-        {/* <Button onClick={handleOpen} variant="outlined" size="large" id="contactBtn" className={classes.button}>Contact us</Button> */}
-        <Button variant="outlined" size="large" id="logoutBtn" className={classes.button} href="./../../../">Log Out</Button>
+        <Button variant="outlined" size="large" id="logoutBtn" className={classes.button} onClick={Middleware.logout}>Log Out</Button>
       </div>
     </div>
   );
@@ -102,31 +65,6 @@ export function RightDrawer(props) {
   return (
     
     <div>
-      {/* <LinkSetting
-      linkSettingOpen = {linkSettingOpen}
-      setLinkSettingOpen = {setLinkSettingOpen}
-      handleClose = {handleClose}
-      links = {links} 
-      setLinks = {setLinks}
-      >
-      </LinkSetting>
-      <WallpaperSetting
-      app={props.app}
-      wallpaperSettingOpen = {wallpaperSettingOpen}
-      setWallpaperSettingOpen = {setWallpaperSettingOpen}
-      handleClose = {handleClose}
-      wallpaper = {wallpaper} 
-      setWallpaper = {setWallpaper}
-      >
-      </WallpaperSetting>
-
-      <ContactSetting
-      contactSettingOpen = {contactSettingOpen}
-      setContactSettingOpen = {setContactSettingOpen}
-      handleClose = {handleClose}
-      >
-      </ContactSetting>
- */}
       <Drawer open={state.right} onClose={() => toggleDrawer('right', state, setState, false)}
         anchor='right'>
         {sideList('right')}
